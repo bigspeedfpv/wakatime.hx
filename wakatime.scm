d395497bfaeb8485f6371055d511e47db3487593
Andrew Brower <bigspeedfpv@gmail.com>
2025-08-04 23:39:48 -0400

    feat: run wakatime sync on offthread

--- wakatime.scm
+++ wakatime.scm
@@ -4,6 +4,7 @@
 (require "steel/result")
 
 (require "helix/static.scm")
+(require "helix/ext.scm")
 
 ; TODO: fallback to wakatime
 (define wakatime-cli "wakatime-cli")
@@ -40,6 +41,7 @@
 ; - the file has been saved
 (define (should-send-file? filename is-file-saved?)
   (or is-file-saved?
+      [not (string=? filename *last-heartbeat-file*)]
       (if (Some? *last-heartbeat-time*)
           [~>
            (instant/now) ;
@@ -48,28 +50,30 @@
            (> same-file-interval)]
           (begin
             (log::info! "wakatime heartbeat hasn't been sent since startup")
-            #true))
-      [not (string=? filename *last-heartbeat-file*)]))
+            #true))))
 
-(define (send-heartbeat is-file-saved?)
-  (if (should-send-file? (cx->current-file) is-file-saved?)
-      (begin
-        (log::info! "sending wakatime heartbeat...")
-        (let* ([file (cx->current-file)]
-               [time (instant/now)]
-               [entity (string-join (list "--entity=\"" file "\""))]
-               [plugin-name "--plugin=\"helix/25.07.1 helix-wakatime/0.1.0\""])
-          (~> (command wakatime-cli [list entity plugin-name])
-              with-stdout-piped
-              spawn-process
-              Ok->value
-              wait->stdout
-              Ok->value)
-          (set! *last-heartbeat-file* file)
-          (set! *last-heartbeat-time* (Some time))))))
+(define (send-heartbeat filename is-file-saved?)
+  (log::info! "sending wakatime heartbeat...")
+  (let* ([time (instant/now)]
+         [entity (string-join (list "--entity=\"" filename "\""))]
+         [plugin-name "--plugin=\"helix/25.07.1 helix-wakatime/0.1.0\""]
+         [write-arg (if is-file-saved? "--write" "")])
+    (~> (command wakatime-cli [list entity plugin-name write-arg])
+        with-stdout-piped
+        spawn-process
+        Ok->value
+        wait->stdout
+        Ok->value)
+    (set! *last-heartbeat-file* filename)
+    (set! *last-heartbeat-time* (Some time))))
 
 ; per Wakatime docs https://wakatime.com/help/creating-plugin#handling-editor-events
 ; updates should be sent when the file is modified, changed, or saved.
+
+(define (heartbeat-off-thread is-file-saved?)
+  (if (should-send-file? (cx->current-file) is-file-saved?)
+      (spawn-native-thread (hx.block-on-task (lambda ()
+                                               (send-heartbeat (cx->current-file) is-file-saved?))))))
 
 (provide register-wakatime)
 (define (register-wakatime)
@@ -78,6 +82,6 @@
     ; This is called literally EVERY time the selection (cursor?) is changed.
     ; I don't think this is the right event to hook but it seems to be the most
     ; suitable one from the 6 or so that I can find
-    (register-hook! 'selection-did-change (lambda (view-id) (send-heartbeat #false)))
-    (register-hook! 'post-insert-char (lambda (event) (send-heartbeat #false)))
-    (register-hook! 'document-saved (lambda (doc-id) (send-heartbeat #true)))))
+    (register-hook! 'selection-did-change (lambda (view-id) (heartbeat-off-thread #false)))
+    (register-hook! 'post-insert-char (lambda (event) (heartbeat-off-thread #false)))
+    (register-hook! 'document-saved (lambda (doc-id) (heartbeat-off-thread #true)))))
